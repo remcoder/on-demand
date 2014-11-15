29fd86ed19c2c9b9e4dda117e051cce14a8bc880
Remco Veldkamp <remcoder@gmail.com>
2014-11-15 22:24:29 +0100

    rewrote async code using fibers/futures

--- src/server/harvest.js
+++ src/server/harvest.js
@@ -1,6 +1,8 @@
 'use strict';
 
+var Future = Npm.require('fibers/future');
 var cheerio = Meteor.npmRequire('cheerio');
+var $ = null;
 
 function autoUpdate() {
 
@@ -19,7 +21,7 @@ function updateIfNeeded() {
 function needsUpdate() {
   var harvest = Harvest.findOne('singleton');
   if (!harvest) {
-      console.log('no previous harvest found')
+      console.log('no previous harvest found');
       Harvest.insert({ _id : 'singleton' });
       return true;
   }
@@ -39,129 +41,145 @@ function harvestFilm1() {
         timestamp: new Date(),
     });
 
-    console.log('harvesting Film1 data')
-    getList(function(movies) {
-        movies.forEach(function(movie){
-            // var doc = Movies.findOne('film1_' + movie.fid);
-            // if (doc) return;
+    console.log('harvesting Film1 data');
+    var before = new Date();
+    var movies = getList().wait();
+    var after = new Date() - before;
+    console.log('\t' + (after/1000).toFixed(1) + 's');
 
-            movie._id = 'film1_' + movie.fid;
+    var futures = movies.map(function(movie){
+        // var doc = Movies.findOne('film1_' + movie.fid);
+        // if (doc) return;
 
-            getDetails(movie.fid, function(details) {
-                _.extend(movie, details);
-                //console.log(movie)
-                Movies.upsert('film1_' + movie.fid, movie);
-            });
+        movie._id = 'film1_' + movie.fid;
 
-
-            getDetails2(movie.url, function(details) {
-
-                //console.log(movie)
-                Movies.upsert('film1_' + movie.fid, { $set : details });
-            });
-        })
+        return getDetailsFut(movie);
     });
+
+    Future.wait.apply(null, futures);
+    after = new Date() - before;
+    console.log('\t' + (after/1000).toFixed(1) + 's');
 }
 
 function _movie(index, li) {
-    var $li = $(li);
-    var idparts = $li.find('a.hover-over').attr('id').split('_');
-    var when = $li.find('h3 + span').text().trim();
-    var nowAvailable = when.toLowerCase() == 'nu te zien';
-    return {
-        fid : idparts[1] ,
-        cover : $li.find('a.hover-over img').attr('src'),
-        url : $li.find('a.hover-over').attr('href'),
-        title: $li.find('h3 a').text(),
-        when : when,
-        nowAvailable : nowAvailable,
-    };
+  var $li = $(li);
+  var idparts = $li.find('a.hover-over').attr('id').split('_');
+  var when = $li.find('h3 + span').text().trim();
+  var nowAvailable = when.toLowerCase() == 'nu te zien';
+  return {
+      fid : idparts[1] ,
+      cover : $li.find('a.hover-over img').attr('src'),
+      url : $li.find('a.hover-over').attr('href'),
+      title: $li.find('h3 a').text(),
+      when : when,
+      nowAvailable : nowAvailable,
+  };
 }
 function getList(fun) {
-    HTTP.get('http://www.film1.nl/film_kijken/film1_on_demand/', function(err, res) {
-        if( err) {
-            console.error(err)
-            console.log(res.content);
-        }
-        $ = cheerio.load(res.content);
+  HTTP.get('http://www.film1.nl/film_kijken/film1_on_demand/', function(err, res) {
+    if( err) {
+        console.error(err);
+        console.log(res.content);
+        fun(err);
+    }
+    $ = cheerio.load(res.content);
 
-        var allMovies = $('.listview').eq(1); // the first listview contains only new titles
-        var rows = allMovies
-            .children('.premiere').get();
+    var allMovies = $('.listview').eq(1); // the first listview contains only new titles
+    var rows = allMovies
+        .children('.premiere').get();
 
+    var flat = rows.reduce(function(acc, cur) {
+        return acc.concat( $(cur).children('li').map(_movie).get() );
+    }, []);
 
-
-        var flat = rows.reduce(function(acc, cur) {
-            return acc.concat( $(cur).children('li').map(_movie).get() );
-        }, []);
-
-        // console.log(flat);
-        fun(flat);
-    });
+    fun(undefined, flat);
+  });
 }
 
-function getDetails(movieId, fun) {
-    HTTP.post('http://www.film1.nl/films/ajax_get_film_info.php', {
-        params: {
-            movie_id: movieId,
-            movie_nr: 0
-        }
-    }, function(err, res) {
-        if( err) {
-            console.error(err)
-            console.log(res.data.html_hoverover);
-        }
+// var getList = Meteor.wrapAsync(getList);
+var getList = Future.wrap(getList);
 
-        $ = cheerio.load(res.data.html_hoverover);
-        var extract = {
-            duration : $('.duration').text(),
-            trailer : $('.movie-trailer').attr('href'),
-            icons : $('.info li img')
-                .map(function(i, el) { return $(el).attr('src'); })
-                .get(),
-            imdb : {
-                rating : $('.imdb-rating .rating-value').text(),
-                votes : $('.imdb-rating .rating-votes').text(),
-                url : $('.imdb-rating .rating-votes').attr('href')
-            },
-            desc : $('.teaser').text()
-        }
+function getDetails1(movie, fun) {
+  var movieId = movie.fid;
+  HTTP.post('http://www.film1.nl/films/ajax_get_film_info.php', {
+      params: {
+          movie_id: movieId,
+          movie_nr: 0
+      }
+  }, function(err, res) {
+      if( err) {
+          console.error(err);
+          console.log(res.data.html_hoverover);
+          fun(err);
+      }
 
-        // console.log(extract);
-        fun(extract);
-    })
+      $ = cheerio.load(res.data.html_hoverover);
+      var details = {
+          duration : $('.duration').text(),
+          trailer : $('.movie-trailer').attr('href'),
+          icons : $('.info li img')
+              .map(function(i, el) { return $(el).attr('src'); })
+              .get(),
+          imdb : {
+              rating : $('.imdb-rating .rating-value').text(),
+              votes : $('.imdb-rating .rating-votes').text(),
+              url : $('.imdb-rating .rating-votes').attr('href')
+          },
+          desc : $('.teaser').text()
+      };
+
+      // Movies.update(movie._id, { $set: details });
+      // console.log(movie.url);
+      fun(undefined, details);
+  });
 }
 
-function getDetails2(url, fun) {
-    console.log('url', url);
-    HTTP.get('http://www.film1.nl' + url, function(err, res) {
-        if (err) {
-            console.error(err);
-            console.log(res);
-        }
 
-        // console.log(res.content);
-        $ = cheerio.load(res.content);
-        var genres = $('.tab-tbl-title:contains(Genre)').next().children('a').map(function(i,el) {
-            return {
-                name: $(el).text(),
-                url: $(el).attr('href'),
-            };
-        }).get();
 
-        genres.forEach(function(g) {
-            var id = 'http://www.film1.nl' + g.url;
-            if (!Genres.findOne(id)) {
-                g._id = id;
-                Genres.upsert(id, g);
-            }
-        });
+function getDetails2(movie, fun) {
+  var url = movie.url;
+  // console.log('url', url);
+  HTTP.get('http://www.film1.nl' + url, function(err, res) {
+      if (err) {
+          console.error(err);
+          console.log(res);
+          fun(err);
+      }
 
-        fun({
-            genres: genres
-        });
-    });
+      // console.log(res.content);
+      $ = cheerio.load(res.content);
+      var genres = $('.tab-tbl-title:contains(Genre)').next().children('a').map(function(i,el) {
+          return {
+              name: $(el).text(),
+              url: $(el).attr('href'),
+          };
+      }).get();
+
+      genres.forEach(function(g) {
+          var id = 'http://www.film1.nl' + g.url;
+          if (!Genres.findOne(id)) {
+              g._id = id;
+              Genres.upsert(id, g);
+          }
+      });
+
+      fun(undefined, {
+          genres: genres
+      });
+  });
 }
+
+
+
+var getDetailsFut = function (movie) {
+  var f1 = Future.wrap(getDetails1)(movie);
+  var f2 = Future.wrap(getDetails2)(movie);
+  var details = _.extend(f1.wait(), f2.wait());
+
+  Movies.upsert(movie._id, { $set: details });
+  console.log(movie.url);
+
+}.future();
 
 var exports = {
     harvestFilm1 : harvestFilm1,
